93e52bbd66a702540c7800f64f237f6e59f95445
谢学典 <1138670400@qq.com>
2019-03-01 19:55:20 +0800

    Add batch extraction SQL support
    Here is a simple example
    
    //Before execution
    ---------------------------------------------------------------------------------------------
    EXEC sp_executesql N'select * from Wd_Admin where Wt_IsDelete=0  AND Wt_IdUser=@Wt_IdUser and Wt_Password=@Wt_Password',N'@Wt_IdUser nvarchar(4000),@Wt_Password nvarchar(4000)',@Wt_IdUser=N'lelelel',@Wt_Password=N'96e79218965eb72c92a549dd5a330112'
    go
    exec sp_executesql N'select * from Wd_AdminType where Wt_IsDelete=0  AND wt_id=@fid',N'@fid int',@fid=4
    go
    exec sp_executesql N'select * from Wd_PartCls where Wt_IsDelete=0  AND Wt_Folder=@Foldername',N'@Foldername nvarchar(4000)',@Foldername=N'Agent'
    go
    exec sp_executesql N'select * from Wd_AdminRight where Wt_IsDelete=0  AND Wt_ParType=1 and Wt_FID=@fid and Wt_ParId=@Wt_ParId ',N'@fid int,@Wt_ParId int',@fid=4,@Wt_ParId=29182
    
    //After execution
    ---------------------------------------------------------------------------------------------
    declare @Wt_IdUser nvarchar(4000),@Wt_Password nvarchar(4000)
    set @Wt_IdUser=N'lelelel'
    set @Wt_Password=N'96e79218965eb72c92a549dd5a330112'
    
    select * from Wd_Admin where Wt_IsDelete=0  AND Wt_IdUser=@Wt_IdUser and Wt_Password=@Wt_Password
    
     GO
    declare @fid int
    set @fid=4
    
    select * from Wd_AdminType where Wt_IsDelete=0  AND wt_id=@fid
    
     GO
    declare @Foldername nvarchar(4000)
    set @Foldername=N'Agent'
    
    select * from Wd_PartCls where Wt_IsDelete=0  AND Wt_Folder=@Foldername
    
     GO
    declare @fid int,@Wt_ParId int
    set @fid=4
    set @Wt_ParId=29182
    select * from Wd_AdminRight where Wt_IsDelete=0  AND Wt_ParType=1 and Wt_FID=@fid and Wt_ParId=@Wt_ParId
    
     GO

--- convert-sp_executesql.cs
+++ convert-sp_executesql.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Linq;
+using System.Windows.Forms;
+using System.Text;
 using System.Text.RegularExpressions;
 
 class Program
@@ -10,28 +12,92 @@ class Program
       
       sqlformatter.exe can be found at http://architectshack.com/PoorMansTSqlFormatter.ashx
     */
+    [STAThreadAttribute]
     static void Main()
-	{
+    {
         var re = new Regex(@"exec*\s*sp_executesql\s+N'([\s\S]*)',\s*N'(@[\s\S]*?)',\s*([\s\S]*)", RegexOptions.IgnoreCase); // 1: the sql, 2: the declare, 3: the setting
-	    var input = Console.In.ReadToEnd();
-	    var match = re.Match(input);
-        if (match.Success)
-        {
-            var sql = match.Groups[1].Value.Replace("''", "'");
-            var declare = match.Groups[2].Value;
-            var setting = match.Groups[3].Value + ',';
-            
-            // to deal with comma or single quote in variable values, we can use the variable name to split
-            var re2 = new Regex(@"@[\s\S]*?\s*=");
-            var variables = re2.Matches(setting).Cast<Match>().Select(m => m.Value).ToArray();
-            var values = re2.Split(setting).Where(s=>!string.IsNullOrWhiteSpace(s)).ToArray();
 
-            Console.WriteLine("BEGIN\nDECLARE {0};", declare);
-            for (int i = 0; i < variables.Length; i++)
+        var rew = new Regex(@"exec*\s*sys.sp_executesql\s+N'([\s\S]*)',\s*N'(@[\s\S]*?)',\s*([\s\S]*)", RegexOptions.IgnoreCase); // 1: the sql, 2: the declare, 3: the setting
+
+        //var input = Console.In.ReadToEnd();
+
+
+
+        //重 剪切板中 获得 要处理的 参数化 SQL
+       var input = Clipboard.GetText();
+
+       
+
+
+        StringBuilder builder = new StringBuilder();
+
+
+        //string[] batch = input.Split(new[] { "Go", "go", "GO" }, StringSplitOptions.None);
+
+
+        //使用 正则表达式 拆分 批量操作
+        //Regex regexs = new Regex(@"Go$",RegexOptions.Multiline|RegexOptions.Singleline|RegexOptions.IgnoreCase|RegexOptions.CultureInvariant);
+
+
+
+
+        var options = System.Text.RegularExpressions.RegexOptions.IgnoreCase | System.Text.RegularExpressions.RegexOptions.Multiline | System.Text.RegularExpressions.RegexOptions.Singleline | System.Text.RegularExpressions.RegexOptions.CultureInvariant;
+
+
+
+        Regex regexs = new Regex(@"\bgo\b", options);
+
+
+        string[] batch = regexs.Split(input);
+
+
+        
+
+        foreach (var item in batch)
+        {
+            var match = re.Match(item);
+
+            if (!match.Success)
             {
-                Console.WriteLine("SET {0}{1};", variables[i], values[i].Substring(0, values[i].Length-1));
+                match = rew.Match(item);
             }
-            Console.WriteLine("{0}\nEND", sql);
+
+            if (match.Success)
+            {
+
+
+                var sql = match.Groups[1].Value.Replace("''", "'");
+                var declare = match.Groups[2].Value;
+                var setting = match.Groups[3].Value + ',';
+
+                // to deal with comma or single quote in variable values, we can use the variable name to split
+                var re2 = new Regex(@"@[\s\S]*?\s*=");
+                var variables = re2.Matches(setting).Cast<Match>().Select(m => m.Value).ToArray();
+                var values = re2.Split(setting).Where(s => !string.IsNullOrWhiteSpace(s)).ToArray();
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = values[i].Trim();
+                }
+
+
+                builder.AppendFormat("BEGIN\nDECLARE {0};\n", declare);
+
+                Console.WriteLine("BEGIN\nDECLARE {0};", declare);
+                for (int i = 0; i < variables.Length; i++)
+                {
+                    builder.AppendFormat("SET {0}{1};\n", variables[i], values[i].Substring(0, values[i].Length - 1));
+                    Console.WriteLine("SET {0}{1};", variables[i], values[i].Substring(0, values[i].Length - 1));
+                }
+                Console.WriteLine("{0}\nEND", sql);
+                builder.AppendFormat("{0}\nEND\n", sql);
+
+                builder.AppendFormat("{0}\n", "Go");
+            }
         }
-	}
+
+        Clipboard.SetText(builder.ToString());
+
+
+    }
 }
